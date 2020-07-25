192cb48e0abd394f3747c2b13015bd4fadf306ed
Jack Toumey <jack@toumey.io>
2020-07-25 01:46:03 -0700

    prettier working

--- packages/web-app/next.config.js
+++ packages/web-app/next.config.js
@@ -1,4 +1,5 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable prettier/prettier */
 
 // Tell webpack to compile "bar", necessary if you're using the export statement for example
 // https://www.npmjs.com/package/next-transpile-modules
